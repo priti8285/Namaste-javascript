303ece9abe0a40e78f1f9a6f41fef95a3c98c299
Priti-Mani <priti.mani@makunaiglobal.ai>
2025-08-18 16:46:06 +0530

    second commit

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 // const heading = React.createElement( "h1", {
 //     id: "heading",
 // }, "Hello World From React !");
@@ -8,7 +10,7 @@ const heading = React.createElement(
   React.createElement(
     "div",
     { id: "child" },
-    React.createElement("h1", {}, "I am H1 TAG!"),
+    React.createElement("h1", {}, "I am Hello TAG!"),
     React.createElement("h2", {}, "I am H2 TAG!")
   )
 );
